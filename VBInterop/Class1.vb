c2cdde0c785bced777204c13970e6fcaceb2d5b3
Divya Aligeti <37445617+AligetiDivya@users.noreply.github.com>
2025-04-25 13:08:44 -0500

    Update and rename Class1.vb to VBHelper.vb

--- VBInterop/Class1.vb
+++
@@ -1,6 +0,0 @@
-Public Class VBHelper
-    Public Shared Function GetNumber() As Integer
-        Return 42
-    End Function
-
-End Class
---
+++ VBInterop/VBHelper.vb
@@ -0,0 +1,11 @@
+
+Namespace VBInterop
+    Public Class VBHelper
+        ''' <summary>
+        ''' Returns a sample integer.
+        ''' </summary>
+        Public Shared Function GetNumber() As Integer
+            Return 42
+        End Function
+    End Class
+End Namespace
